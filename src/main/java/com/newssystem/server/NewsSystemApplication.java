d17979d8e7068822e91cbe99e8921c20375fcec0
Wojciech Zieleziecki <wojciech.zieleziecki@gmail.com>
2017-03-23 17:15:26 +0100

    Communication with MongoDB

--- src/main/java/com/newssystem/server/NewsSystemApplication.java
+++ src/main/java/com/newssystem/server/NewsSystemApplication.java
@@ -2,8 +2,12 @@ package com.newssystem.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan
 public class NewsSystemApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/newssystem/server/config/DatabaseConfiguration.java
@@ -0,0 +1,41 @@
+package com.newssystem.server.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * Created by wzielezi on 2017-03-23.
+ */
+@Configuration
+@EnableMongoRepositories("com.newssystem.server.repository")
+public class DatabaseConfiguration extends AbstractMongoConfiguration {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient(host + ":" + port);
+    }
+}
